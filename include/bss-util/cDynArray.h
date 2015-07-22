58a02b8f1162653f8a0a971b01932794e88151ba
Erik McClure <erikm@blackspherestudios.com>
2015-07-22 02:53:27 -0700

    project file commit

---
+++ include/bss-util/cDynArray.h
@@ -0,0 +1,156 @@
+// Copyright ©2015 Black Sphere Studios
+// For conditions of distribution and use, see copyright notice in "bss_util.h"
+
+#ifndef __C_DYN_ARRAY_H__BSS__
+#define __C_DYN_ARRAY_H__BSS__
+
+#include "cArray.h"
+
+namespace bss_util {
+  // Dynamic array implemented using ArrayType (should only be used when constructors could potentially not be needed)
+  template<class T, typename SizeType=unsigned int, ARRAY_TYPE ArrayType = CARRAY_SIMPLE, typename Alloc=StaticAllocPolicy<T>>
+  class BSS_COMPILER_DLLEXPORT cDynArray : protected cArrayBase<T, SizeType, ArrayType, Alloc>
+  {
+  protected:
+    typedef cArrayBase<T, SizeType, ArrayType, Alloc> AT_;
+    typedef typename AT_::ST_ ST_;
+    typedef typename AT_::T_ T_;
+    using AT_::_array;
+    using AT_::_size;
+
+  public:
+    inline cDynArray(const cDynArray& copy) : AT_(copy), _length(copy._length) {}
+    inline cDynArray(cDynArray&& mov) : AT_(std::move(mov)), _length(mov._length) {}
+    inline explicit cDynArray(ST_ size=0) : AT_(size), _length(0) {}
+    inline cDynArray(const std::initializer_list<T> list) : AT_(list), _length(list.size()) {}
+    BSS_FORCEINLINE ST_ Add(const T_& t) { return _add(t); }
+    BSS_FORCEINLINE ST_ Add(T_&& t) { return _add(std::move(t)); }
+    BSS_FORCEINLINE void Remove(ST_ index) { AT_::RemoveInternal(index); --_length; }
+    BSS_FORCEINLINE void RemoveLast() { --_length; }
+    BSS_FORCEINLINE void Insert(const T_& t, ST_ index=0) { return _insert(t, index); }
+    BSS_FORCEINLINE void Insert(T_&& t, ST_ index=0) { return _insert(std::move(t), index); }
+    BSS_FORCEINLINE bool Empty() const { return !_length; }
+    BSS_FORCEINLINE void Clear() { _length=0; }
+    BSS_FORCEINLINE void SetLength(ST_ length) { if(length>_size) AT_::SetSize(length); _length=length; }
+    BSS_FORCEINLINE void Reserve(ST_ capacity) { if(capacity>_size) AT_::SetSize(capacity); }
+    BSS_FORCEINLINE ST_ Length() const { return _length; }
+    BSS_FORCEINLINE ST_ Capacity() const { return _size; }
+    inline const T_& Front() const { assert(_length>0); return _array[0]; }
+    inline const T_& Back() const { assert(_length>0); return _array[_length-1]; }
+    inline T_& Front() { assert(_length>0); return _array[0]; }
+    inline T_& Back() { assert(_length>0); return _array[_length-1]; }
+    inline const T_* begin() const { return _array; }
+    inline const T_* end() const { return _array+_length; }
+    inline T_* begin() { return _array; }
+    inline T_* end() { return _array+_length; }
+
+    inline operator T_*() { return _array; }
+    inline operator const T_*() const { return _array; }
+    inline cDynArray& operator=(const AT_& copy) { AT_::operator=(copy); _length=_size; return *this; }
+    inline cDynArray& operator=(AT_&& mov) { AT_::operator=(std::move(mov)); _length=_size; return *this; }
+    inline cDynArray& operator=(const cDynArray& copy) { AT_::operator=(copy); _length=copy._length; return *this; }
+    inline cDynArray& operator=(cDynArray&& mov) { AT_::operator=(std::move(mov)); _length=mov._length; return *this; }
+    inline cDynArray& operator +=(const cDynArray& add) { AT_::SetSize(_length); AT_::operator+=(add); _length+=add._length; return *this; }
+    inline cDynArray operator +(const cDynArray& add) const { cDynArray r(*this); r+=add; return r; }
+
+  protected:
+    template<typename U>
+    ST_ _add(U && t) { _checksize(); _array[_length]=std::forward<U>(t); return _length++; }
+    template<typename U>
+    void _insert(U && t, ST_ index=0) { _checksize(); AT_::_pushback(index, (_length++)-index, std::forward<U>(t)); assert(_length<=_size); }
+    BSS_FORCEINLINE void _checksize()
+    {
+      if(_length>=_size) AT_::SetSize(T_FBNEXT(_size));
+      assert(_length<_size);
+    }
+
+    ST_ _length;
+  };
+
+  // A dynamic array that can dynamically adjust the size of each element
+  template<typename ST_, typename Alloc=StaticAllocPolicy<unsigned char>>
+  class BSS_COMPILER_DLLEXPORT cArbitraryArray : protected cArrayBase<unsigned char, ST_, CARRAY_SIMPLE, Alloc>
+  {
+  protected:
+    typedef cArrayBase<unsigned char, ST_, CARRAY_SIMPLE, Alloc> AT_;
+    using AT_::_array;
+    using AT_::_size;
+
+  public:
+    inline cArbitraryArray(const cArbitraryArray& copy) : AT_(copy), _length(copy._length), _element(copy._element) {}
+    inline cArbitraryArray(cArbitraryArray&& mov) : AT_(std::move(mov)), _length(mov._length), _element(mov._element) {}
+    inline cArbitraryArray(ST_ size=0, ST_ element=1): AT_(size*element), _length(0), _element(element) {}
+    template<typename T>
+    inline ST_ Add(const T& t)
+    {
+      if((_length*_element)>=_size) AT_::SetSize(T_FBNEXT(_length)*_element);
+      memcpy(_array+(_length*_element), &t, bssmin(sizeof(T), _element));
+      return _length++;
+    }
+    inline void Remove(ST_ index)
+    {
+      index *= _element;
+      assert(_size>0 && index<_size);
+      memmove(_array+index, _array+index+_element, _size-index-_element);
+      --_length;
+    }
+    inline void RemoveLast() { --_length; }
+    inline void SetElement(const void* newarray, ST_ element, ST_ num) // num is a count of how many elements are in the array
+    {
+      if(((unsigned char*)newarray)==_array) return;
+      _element=element;
+      _length=num;
+      if((_length*_element)>_size) AT_::SetSizeDiscard(_length*_element);
+      if(_array)
+        memcpy(_array, newarray, element*num);
+    }
+    template<typename T> // num is a count of how many elements are in the array
+    BSS_FORCEINLINE void SetElement(const T* newarray, ST_ num) { SetElement(newarray, sizeof(T), num); }
+    template<typename T, unsigned int NUM>
+    BSS_FORCEINLINE void SetElement(const T(&newarray)[NUM]) { SetElement(newarray, sizeof(T), NUM); }
+    void SetElement(ST_ element)
+    {
+      if(element==_element) return;
+      _size=element*_length;
+      unsigned char* narray = !_length?0:(unsigned char*)Alloc::allocate(_size);
+      memset(narray, 0, _size);
+      ST_ m=bssmin(element, _element);
+      for(unsigned int i = 0; i < _length; ++i)
+        memcpy(narray+(i*element), _array+(i*_element), m);
+      if(_array)
+        Alloc::deallocate(_array);
+      _array=narray;
+      _element=element;
+      assert(element>0);
+    }
+    inline ST_ Element() const { return _element; }
+    inline bool Empty() const { return !_length; }
+    inline void Clear() { _length=0; }
+    inline void SetLength(ST_ length) { _length=length; length*=_element; if(length>_size) AT_::SetSize(length); }
+    inline ST_ Length() const { return _length; }
+    // These are templatized accessors, but we don't do an assertion on if the size of T is the same as _element, because there are many cases where this isn't actually true.
+    template<typename T> inline const T& Front() const { assert(_length>0); return _array[0]; }
+    template<typename T> inline const T& Back() const { assert(_length>0); return _array[(_length-1)*_element]; }
+    template<typename T> inline T& Front() { assert(_length>0); return _array[0]; }
+    template<typename T> inline T& Back() { assert(_length>0); return _array[(_length-1)*_element]; }
+    template<typename T> inline const T* begin() const { return _array; }
+    template<typename T> inline const T* end() const { return _array+(_length*_element); }
+    template<typename T> inline T* begin() { return _array; }
+    template<typename T> inline T* end() { return _array+(_length*_element); }
+    template<typename T> inline const T& Get(ST_ index) const { return *(T*)Get(index); }
+    template<typename T> inline T& Get(ST_ index) { return *(T*)Get(index); }
+    BSS_FORCEINLINE const void* Get(ST_ index) const { assert(index<_length); return (_array+(index*_element)); }
+    BSS_FORCEINLINE void* Get(ST_ index) { assert(index<_length); return (_array+(index*_element)); }
+
+    inline const void* operator[](ST_ index) const { return Get(index); }
+    inline void* operator[](ST_ index) { return Get(index); }
+    inline cArbitraryArray& operator=(const cArbitraryArray& copy) { AT_::operator=(copy); _length=copy._length; _element=copy._element; return *this; }
+    inline cArbitraryArray& operator=(cArbitraryArray&& mov) { AT_::operator=(std::move(mov)); _length=mov._length; _element=mov._element; return *this; }
+
+  protected:
+    ST_ _length; // Total length of the array in number of elements
+    ST_ _element; // Size of each element
+  };
+}
+
+#endif
---
+++ include/bss-util/cJSON.h
@@ -0,0 +1,305 @@
+// Copyright ©2015 Black Sphere Studios
+// For conditions of distribution and use, see copyright notice in "bss_util.h"
+
+#ifndef __C_JSON_H__BSS__
+#define __C_JSON_H__BSS__
+
+#include "cDynArray.h"
+#include "cStr.h"
+#include "bss_util.h"
+#include <sstream>
+#include <istream>
+#include <ostream>
+
+namespace bss_util {
+  static inline void ParseJSONEatWhitespace(std::istream& s) { while(!!s && isspace(s.peek())) s.get(); }
+  static inline void ParseJSONEatCharacter(std::string& str, std::istream& src) // this processes escape characters when reading through strings
+  {
+    char c = src.get();
+    if(c != '\\')
+    {
+      str += c;
+      return;
+    }
+    switch(src.get())
+    {
+    case '"': str += '"'; break;
+    case '\\': str += '\\'; break;
+    case '/': str += '/'; break;
+    case 'b': str += '\b'; break;
+    case 'f': str += '\f'; break;
+    case 'n': str += '\n'; break;
+    case 'r': str += '\r'; break;
+    case 't': str += '\t'; break;
+    case 'u':
+      char buf[5];
+      for(int i = 0; i < 4; ++i) buf[i] = src.get();
+      buf[4] = 0;
+      OutputUnicode(str, strhex(buf));
+      break;
+    }
+  }
+
+  DEFINE_MEMBER_CHECKER(EvalJSON);
+
+  template<class T, bool B>
+  struct ParseJSONInternal
+  {
+    static_assert(HAS_MEMBER(T, EvalJSON), "T must implement void EvalJSON(const char*, std::istream&)");
+    static void F(T& obj, std::istream& s)
+    {
+      cStr buf;
+      ParseJSONEatWhitespace(s);
+      if(!s || s.get() != '{') return;
+      ParseJSONEatWhitespace(s);
+      while(!!s && s.peek() != '}' && s.peek() != -1)
+      {
+        if(!s || s.get() != '"' || s.peek() == -1) continue;
+        buf.clear(); // clear buffer to hold name
+        while(!!s && s.peek() != '"' && s.peek() != -1) ParseJSONEatCharacter(buf, s);
+        if(s) s.get(); // eat " character
+        ParseJSONEatWhitespace(s);
+        if(s.get() != ':') continue;
+        ParseJSONEatWhitespace(s);
+        obj.EvalJSON(buf.c_str(), s);
+        while(!!s && s.peek() != ',' && s.peek() != '}' && s.peek() != -1) s.get(); // eat everything up to a , or } character
+        if(!!s && s.peek() == ',') s.get(); // Only eat comma if it's there.
+        ParseJSONEatWhitespace(s);
+      }
+      if(s.peek() == '}') s.get(); // eat the closing brace
+    }
+  };
+  template<class T> // For arithmetic base types
+  struct ParseJSONInternal<T, true>
+  {
+    static void F(T& obj, std::istream& s)
+    {
+      if(s.peek() == ',' || s.peek() == ']' || s.peek() == '}') return;
+      if(s.peek() == '"') // if true, we have to attempt to coerce the string to T
+      {
+        s.get();
+        s >> obj; // grab whatever we can
+        while(!!s && s.peek() != -1 && s.get() != '"'); // eat the rest of the string
+        s.get(); // eat the " character
+      } else
+        s >> obj;
+    }
+  };
+  template<class T>
+  void ParseJSONArray(T& obj, std::istream& s)
+  {
+    ParseJSONEatWhitespace(s);
+    if(!s || s.get() != '[') return;
+    ParseJSONEatWhitespace(s);
+    int n = 0;
+    while(!!s && s.peek() != ']' && s.peek() != -1)
+    {
+      ParseJSONInternal<T, false>::DoAddCall(obj, s, n);
+      while(!!s && s.peek() != ',' && s.peek() != ']' && s.peek() != -1) s.get(); // eat everything up to a , or ] character
+      if(!!s && s.peek() == ',' && s.peek() != -1) s.get(); // Only eat comma if it's there.
+      ParseJSONEatWhitespace(s);
+    }
+  }
+  template<class T, typename SizeType, ARRAY_TYPE ArrayType, typename Alloc>
+  struct ParseJSONInternal<cDynArray<T, SizeType, ArrayType, Alloc>, false>
+  {
+    static inline void DoAddCall(cDynArray<T, SizeType, ArrayType, Alloc>& obj, std::istream& s, int& n)
+    {
+      obj.Add(T());
+      ParseJSON(obj.Back(), s);
+    }
+    static void F(cDynArray<T, SizeType, ArrayType, Alloc>& obj, std::istream& s)
+    {
+      obj.Clear();
+      ParseJSONArray<cDynArray<T, SizeType, ArrayType, Alloc>>(obj, s);
+    }
+  };
+  template<class T, int I, bool B> // For fixed-length arrays
+  struct ParseJSONInternal<T[I], B>
+  {
+    static inline void DoAddCall(T(&obj)[I], std::istream& s, int& n) { if(n<I) ParseJSON(obj[n++], s); }
+    static void F(T(&obj)[I], std::istream& s) { ParseJSONArray<T[I]>(obj, s); }
+  };
+  template<class T, typename Alloc>
+  struct ParseJSONInternal<std::vector<T, Alloc>, false>
+  {
+    static inline void DoAddCall(std::vector<T, Alloc>& obj, std::istream& s, int& n)
+    {
+      obj.push_back(T());
+      ParseJSON(obj.back(), s);
+    }
+    static void F(std::vector<T, Alloc>& obj, std::istream& s)
+    {
+      obj.clear();
+      ParseJSONArray<std::vector<T, Alloc>>(obj, s);
+    }
+  };
+
+  template<class T>
+  inline void ParseJSON(T& obj, std::istream& s){ ParseJSONInternal<T, std::is_arithmetic<T>::value>::F(obj, s); }
+  template<class T>
+  inline void ParseJSON(T& obj, const char* s){ std::istringstream ss(s); ParseJSON<T>(obj, ss); }
+
+  template<>
+  inline void ParseJSON<std::string>(std::string& target, std::istream& s)
+  {
+    target.clear();
+    if(s.peek() == ',' || s.peek() == ']' || s.peek() == '}') return;
+    char c = s.get();
+    if(c != '"')
+    {
+      target += c;
+      while(!!s && s.peek() != ',' && s.peek() != '}' && s.peek() != ']' && s.peek() != -1) target += s.get(); 
+      return;
+    } 
+    
+    while(!!s && s.peek() != '"' && s.peek() != -1)
+      ParseJSONEatCharacter(target, s);
+    s.get(); // eat last " character
+  }
+  template<>
+  inline void ParseJSON<cStr>(cStr& target, std::istream& s) { ParseJSON<std::string>(target, s); }
+  template<>
+  inline void ParseJSON<bool>(bool& target, std::istream& s)
+  {
+    static const char* val = "true";
+    int pos = 0;
+    if(s.peek() >= '0' && s.peek() <= '9') 
+    {
+      unsigned __int64 num;
+      s >> num;
+      target = num != 0; // If it's numeric, record the value as false if 0 and true otherwise.
+      return;
+    }
+    while(!!s && s.peek() != ',' && s.peek() != '}' && s.peek() != ']' && s.peek() != -1 && pos < 4)
+    {
+      if(s.get() != val[pos++])
+      {
+        target = false;
+        return;
+      }
+    }
+
+    target = true;
+  }
+
+  DEFINE_MEMBER_CHECKER(SerializeJSON);
+
+  static bool WriteJSONIsPretty(unsigned int pretty) { return (pretty&(~0x80000000))>0; }
+  static void WriteJSONTabs(std::ostream& s, unsigned int pretty) { unsigned int count = (pretty&(~0x80000000))-1; for(unsigned int i = 0; i < count; ++i) s << '\t'; }
+  static void WriteJSONId(const char* id, std::ostream& s, unsigned int pretty) { if(id) { if(WriteJSONIsPretty(pretty)) { s << std::endl; WriteJSONTabs(s, pretty); } s << '"' << id << '"' << ": "; } }
+  static void WriteJSONComma(std::ostream& s, unsigned int& pretty)
+  {
+    if(pretty&0x80000000) s << ',';
+    pretty|=0x80000000;
+  }
+  static unsigned int WriteJSONPretty(unsigned int pretty) { return (pretty&(~0x80000000)) + ((pretty&(~0x80000000))>0); }
+
+  template<class T, bool B>
+  struct WriteJSONInternal
+  {
+    static_assert(HAS_MEMBER(T, SerializeJSON), "T must implement void SerializeJSON(std::istream&, unsigned int&) const");
+    static void F(const char* id, const T& obj, std::ostream& s, unsigned int& pretty)
+    {
+      WriteJSONComma(s, pretty);
+      if(!id && WriteJSONIsPretty(pretty)) s << std::endl;
+      WriteJSONId(id, s, pretty);
+      if(!id && WriteJSONIsPretty(pretty)) WriteJSONTabs(s, pretty);
+      s << '{';
+      unsigned int npretty = WriteJSONPretty(pretty);
+      obj.SerializeJSON(s, npretty);
+      if(WriteJSONIsPretty(pretty)) {
+        s << std::endl;
+        WriteJSONTabs(s, pretty);
+      }
+      s << '}';
+      s.flush();
+    }
+  };
+  
+  template<class T>
+  struct WriteJSONInternal<T, true>
+  {
+    static void F(const char* id, const T& obj, std::ostream& s, unsigned int& pretty)
+    {
+      WriteJSONComma(s, pretty);
+      WriteJSONId(id, s, pretty);
+      s << obj;
+    }
+  };
+
+  template<class T>
+  void WriteJSONArray(const char* id, const T* obj, size_t size, std::ostream& s, unsigned int& pretty)
+  {
+    WriteJSONComma(s, pretty);
+    WriteJSONId(id, s, pretty);
+    unsigned int npretty = WriteJSONPretty(pretty);
+    s << '[';
+    for(int i = 0; i < size; ++i)
+      WriteJSON(0, obj[i], s, npretty);
+    s << ']';
+  }
+
+  template<class T, int I, bool B>
+  struct WriteJSONInternal<T[I], B>
+  {
+    static void F(const char* id, const T(&obj)[I], std::ostream& s, unsigned int& pretty) { WriteJSONArray<T>(id, obj, I, s, pretty); }
+  };
+  template<class T, typename SizeType, ARRAY_TYPE ArrayType, typename Alloc>
+  struct WriteJSONInternal<cDynArray<T, SizeType, ArrayType, Alloc>, false>
+  {
+    static void F(const char* id, const cDynArray<T, SizeType, ArrayType, Alloc>& obj, std::ostream& s, unsigned int& pretty) { WriteJSONArray<T>(id, obj, obj.Length(), s, pretty); }
+  };
+  template<class T, typename Alloc>
+  struct WriteJSONInternal<std::vector<T, Alloc>, false>
+  {
+    static void F(const char* id, const std::vector<T, Alloc>& obj, std::ostream& s, unsigned int& pretty) { WriteJSONArray<T>(id, obj, obj.size(), s, pretty); }
+  };
+
+  // To enable pretty output, set pretty to 1. The upper two bits are used as flags, so if you need more than 1073741823 levels of indentation... what the hell are you doing?!
+  template<class T> 
+  void WriteJSON(const char* id, const T& obj, std::ostream& s, unsigned int& pretty) { WriteJSONInternal<T, std::is_arithmetic<T>::value>::F(id, obj, s, pretty); }
+
+  template<>
+  void WriteJSON<std::string>(const char* id, const std::string& obj, std::ostream& s, unsigned int& pretty)
+  {
+    WriteJSONComma(s, pretty);
+    WriteJSONId(id, s, pretty);
+    s << '"';
+    for(int i = 0; i < obj.size(); ++i)
+    {
+      switch(obj[i])
+      {
+      case '"': s << "\\\""; break;
+      case '\\': s << "\\\\"; break;
+      case '/': s << "\\/"; break;
+      case '\b': s << "\\b"; break;
+      case '\f': s << "\\f"; break;
+      case '\n': s << "\\n"; break;
+      case '\r': s << "\\r"; break;
+      case '\t': s << "\\t"; break;
+      default: s << obj[i]; break;
+      }
+    }
+    s << '"';
+  }
+
+  template<>
+  void WriteJSON<cStr>(const char* id, const cStr& obj, std::ostream& s, unsigned int& pretty) { WriteJSON<std::string>(id, obj, s, pretty); }
+
+  template<>
+  void WriteJSON<bool>(const char* id, const bool& obj, std::ostream& s, unsigned int& pretty)
+  {
+    WriteJSONComma(s, pretty);
+    WriteJSONId(id, s, pretty);
+    s << (obj?"true":"false");
+  }
+
+  template<class T>
+  void WriteJSON(const T& obj, std::ostream& s, unsigned int pretty = 0) { WriteJSON<T>(0, obj, s, pretty); }
+
+  template<class T>
+  void WriteJSON(const T& obj, const char* file, unsigned int pretty = 0) { std::ofstream fs(file, std::ios_base::out | std::ios_base::trunc | std::ios_base::binary); WriteJSON<T>(0, obj, fs, pretty); }
+}
+
+#endif
